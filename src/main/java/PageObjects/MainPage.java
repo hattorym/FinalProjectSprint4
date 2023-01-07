81bf202a11671b8ee966bc7d20f83b7705dff288
hattorym <qa.iam.sab@gmail.com>
2023-01-08 01:30:18 +0300

    MainPage change. Added Arrays and metods

--- src/main/java/PageObjects/MainPage.java
+++ src/main/java/PageObjects/MainPage.java
@@ -2,13 +2,18 @@ package PageObjects;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import static org.junit.Assert.assertEquals;
+
+// Элементы главной страницы сайта:
 public class MainPage {
-    public MainPage(WebDriver webDriver) {
-        this.webDriver = webDriver;
+    public MainPage(WebDriver driver) {
+        this.driver = driver;
     }
-// Элементы главной страницы сайта
-    private final WebDriver webDriver;
+
+    private WebDriver driver;
 
     // Кнопка заказа в хэдере сайта
     private By headerOrderButton = By.className("Button_Button__ra12g");
@@ -16,39 +21,83 @@ public class MainPage {
     // Кнопка заказа в середине сайта
     private By middleOrderButton = By.className("Button_Button__ra12g Button_Middle__1CSJM");
 
-    // Раздел "Вопросы о важном", стрелочки вопросов
-    private By accordionArrow48 = By.id("accordion__heading-48");
-    private By accordionArrow49 = By.id("accordion__heading-49");
-    private By accordionArrow50 = By.id("accordion__heading-50");
-    private By accordionArrow51 = By.id("accordion__heading-51");
-    private By accordionArrow52 = By.id("accordion__heading-52");
-    private By accordionArrow53 = By.id("accordion__heading-53");
-    private By accordionArrow54 = By.id("accordion__heading-54");
-    private By accordionArrow55 = By.id("accordion__heading-55");
+    // Кнопка "да все привыкли" сообщения о кукисах
+    private By cookieButton = By.id("rcc-confirm-button");
 
-    // Вопросы о важном, ответы
-    private By accordionItemPanel48 = By.id("accordion__panel-48");
-    private By accordionItemPanel49 = By.id("accordion__panel-49");
-    private By accordionItemPanel50 = By.id("accordion__panel-50");
-    private By accordionItemPanel51 = By.id("accordion__panel-51");
-    private By accordionItemPanel52 = By.id("accordion__panel-52");
-    private By accordionItemPanel53 = By.id("accordion__panel-53");
-    private By accordionItemPanel54 = By.id("accordion__panel-54");
-    private By accordionItemPanel55 = By.id("accordion__panel-55");
+    // Лого Яндекс в хэдере
+    private By logoYandexHeader = By.className("Header_LogoYandex__3TSOI");
+
+    // Лого Самокат в хэдере
+    private By logoScooterHeader = By.className("Header_LogoScooter__3lsAR");
+
+    // Раздел "Вопросы о важном", массив локаторов стрелочек вопросов
+    private String[] accordionArrowsArray = new String[]{
+            "accordion__heading-0",
+            "accordion__heading-1",
+            "accordion__heading-2",
+            "accordion__heading-3",
+            "accordion__heading-4",
+            "accordion__heading-5",
+            "accordion__heading-6",
+            "accordion__heading-7"
+    };
+    // Раздел "Вопросы о важном", массив локаторов панелей с текстом ответов
+    private String[] accordionItemPanelArray = new String[]{
+            "accordion__panel-0",
+            "accordion__panel-1",
+            "accordion__panel-2",
+            "accordion__panel-3",
+            "accordion__panel-4",
+            "accordion__panel-5",
+            "accordion__panel-6",
+            "accordion__panel-7"
+    };
+
 
 // Методы для работы с элементами главной страницы
 
-    //Открыть сайт
+    // Открыть сайт
     public MainPage openSite() {
-        webDriver.get("https://qa-scooter.praktikum-services.ru/");
+        driver.get("https://qa-scooter.praktikum-services.ru/");
         return this;
     }
+    // Кликнуть по кнопке заказа в хэдере сайта
+
     public MainPage clickHeaderOrderButton() {
-        webDriver.findElement(headerOrderButton).click();
+        driver.findElement(headerOrderButton).click();
         return this;
     }
+    // Кликнуть по кнопке заказа в середине сайта
     public MainPage clickMiddleOrderButton() {
-        webDriver.findElement(middleOrderButton).click();
+        driver.findElement(middleOrderButton).click();
         return this;
+    }
+    // Кликнуть по кнопке "да все привыкли" сообщения о кукисах
+    public MainPage clickCookieButton() {
+        driver.findElement(cookieButton).click();
+        return this;
+    }
+    // Кликнуть по лого Яндекс в хэдере
+    public MainPage clickLogoYandexHeader() {
+        driver.findElement(logoYandexHeader).click();
+        return this;
+    }
+    // Кликнуть по лого Самокат в хэдере
+    public MainPage clickLogoScooterHeader() {
+        driver.findElement(logoScooterHeader).click();
+        return this;
+    }
+    // Кликнуть по стрелочке вопросов по списку
+    public void clickAccordionArrow(int arrowNumber) {
+        new WebDriverWait(driver, 5)
+                .until(ExpectedConditions.elementToBeClickable(By.id(accordionArrowsArray[arrowNumber])));
+        driver.findElement(By.id(accordionArrowsArray[arrowNumber])).click();
+    }
+    // Сравнить текст ответа с вопросом
+    public void compareAnswerWithQuestionTextAccordionItemPanel(String accordionQuestionText, int panelNumber) {
+        new WebDriverWait(driver, 5)
+                .until(ExpectedConditions.visibilityOfElementLocated(By.id(accordionItemPanelArray[panelNumber])));
+        String accordionAnswerText = driver.findElement(By.id(accordionItemPanelArray[panelNumber])).getText();
+        assertEquals(accordionQuestionText, accordionAnswerText);
     }
 }
