887eeab8029f184d89b752c366b6c9f335cf965a
hattorym <qa.iam.sab@gmail.com>
2023-01-08 23:49:28 +0300

    Done TestSamokatOrdering

--- src/test/java/TestSamokatOrdering.java
+++ src/test/java/TestSamokatOrdering.java
@@ -3,6 +3,8 @@ import PageObjects.OrderPage;
 import PageObjects.RentPage;
 import org.junit.Test;
 
+import static org.junit.Assert.assertTrue;
+
 public class TestSamokatOrdering extends CommonBaseTest {
 
     // Тест заказа через кнопку в хэдере
@@ -21,8 +23,15 @@ public class TestSamokatOrdering extends CommonBaseTest {
                 .sendDeliveryClientPhoneNumber("83223223322")
                 .clickNextButton();
 
-//        new RentPage(driver)
-//                .
+        boolean isDisplayed = new RentPage(driver)
+                .sendRentalDate("28.03.2025")
+                .setRentalTime()
+                .clickCheckBoxColourBlackPearl()
+                .sendComment("Таки не поцарапайте мне аппарат, я вас прошу!")
+                .clickOrderButton()
+                .clickOrderButtonYes()
+                .isModalOrderWindowDisplayed();
+        assertTrue("Шо значит не получилось оформить! Я на вас в суд по правам человека подам!", isDisplayed);
     }
     // Тест заказа через кнопку в середине страницы
     @Test
@@ -40,7 +49,14 @@ public class TestSamokatOrdering extends CommonBaseTest {
                 .sendDeliveryClientPhoneNumber("87776996969")
                 .clickNextButton();
 
-//        new RentPage(driver)
-//                .
+        boolean isDisplayed = new RentPage(driver)
+                .sendRentalDate("07.01.2023")
+                .setRentalTime()
+                .clickCheckBoxColourGreyDespair()
+                .sendComment("Сэрый мапэд хачу!")
+                .clickOrderButton()
+                .clickOrderButtonYes()
+                .isModalOrderWindowDisplayed();
+        assertTrue("Нэт окна заказа, нэ купил мапэд!", isDisplayed);
     }
 }
